d09c991f7dc36965772732c60158b86913c43a7d
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2022-09-26 16:21:03 +0100

    no plot update

--- ourmodel.R
+++ ourmodel.R
@@ -93,60 +93,60 @@ bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, ns
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-fitY <- bi_lst$y %>% 
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup() %>%
-  left_join(y %>% rename(Y = value))
-
-
-g1 <- ggplot(data = fitY) +
-  geom_ribbon(aes(x = time, ymin = q25, ymax = q75),alpha=0.3) +
-  geom_ribbon(aes(x = time, ymin = q025, ymax = q975),alpha=0.3) +
-  geom_line(aes(x = time, y = q50)) +
-  geom_point(aes(x = time, y = Y), colour = "Red") +
-  ylab("Daily new confirmed cases") +
-  xlab("Time-Day")
-
-plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup()
-
-g2 <- ggplot(data = plot_df) +
-  geom_ribbon(aes(x = time, ymin = q25, ymax = q75),alpha = 0.3) +
-  geom_ribbon(aes(x = time, ymin = q025, ymax = q975),alpha = 0.3) +
-  geom_line(aes(x = time, y = q50)) +
-  ylab(TeX("Transmission rate ($\\beta(t)$)")) +
-  xlab("Time-Day")
-
-plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>% 
-  group_by(np) %>% mutate(value = value - value[1]) %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup()
-
-g3 <- ggplot(data = plot_df) +
-  geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
-  geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
-  geom_line(aes(x = time, y = q50)) +
-  ylab(TeX("Relative transmission rate. ($\\beta(t)-\\beta(0)$)")) +
-  xlab("Time-Day")
-
-
-ggarrange(g1, g2, g3, ncol = 1, nrow = 3, align = "v")
+# fitY <- bi_lst$y %>% 
+#   group_by(time) %>%
+#  mutate(
+#     q025 = quantile(value, 0.025),
+#     q25 = quantile(value, 0.25),
+#     q50 = quantile(value, 0.5),
+#     q75 = quantile(value, 0.75),
+#     q975 = quantile(value, 0.975)
+#   ) %>% ungroup() %>%
+#   left_join(y %>% rename(Y = value))
+# 
+# 
+# g1 <- ggplot(data = fitY) +
+#   geom_ribbon(aes(x = time, ymin = q25, ymax = q75),alpha=0.3) +
+#   geom_ribbon(aes(x = time, ymin = q025, ymax = q975),alpha=0.3) +
+#   geom_line(aes(x = time, y = q50)) +
+#   geom_point(aes(x = time, y = Y), colour = "Red") +
+#   ylab("Daily new confirmed cases") +
+#   xlab("Time-Day")
+# 
+# plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
+#   group_by(time) %>%
+#   mutate(
+#     q025 = quantile(value, 0.025),
+#     q25 = quantile(value, 0.25),
+#     q50 = quantile(value, 0.5),
+#     q75 = quantile(value, 0.75),
+#     q975 = quantile(value, 0.975)
+#   ) %>% ungroup()
+# 
+# g2 <- ggplot(data = plot_df) +
+#   geom_ribbon(aes(x = time, ymin = q25, ymax = q75),alpha = 0.3) +
+#   geom_ribbon(aes(x = time, ymin = q025, ymax = q975),alpha = 0.3) +
+#   geom_line(aes(x = time, y = q50)) +
+#   ylab(TeX("Transmission rate ($\\beta(t)$)")) +
+#   xlab("Time-Day")
+# 
+# plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>% 
+#   group_by(np) %>% mutate(value = value - value[1]) %>%
+#   group_by(time) %>%
+#   mutate(
+#     q025 = quantile(value, 0.025),
+#     q25 = quantile(value, 0.25),
+#     q50 = quantile(value, 0.5),
+#     q75 = quantile(value, 0.75),
+#     q975 = quantile(value, 0.975)
+#   ) %>% ungroup()
+# 
+# g3 <- ggplot(data = plot_df) +
+#   geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
+#   geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
+#   geom_line(aes(x = time, y = q50)) +
+#   ylab(TeX("Relative transmission rate. ($\\beta(t)-\\beta(0)$)")) +
+#   xlab("Time-Day")
+# 
+# 
+# ggarrange(g1, g2, g3, ncol = 1, nrow = 3, align = "v")
