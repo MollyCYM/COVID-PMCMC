4437d550ba7cd5c008e9409fa78c1bc7cfe2d678
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2022-10-11 15:11:44 +0100

    update1

--- papermodel.R
+++ papermodel.R
@@ -106,42 +106,42 @@ bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, ns
 
 bi_lst <- bi_read(bi %>% sample_obs)
 # fitY <- bi_lst$y %>% 
-#   group_by(time) %>%
-#   mutate(
-#     q025 = quantile(value, 0.025),
-#     q25 = quantile(value, 0.25),
-#     q50 = quantile(value, 0.5),
-#     q75 = quantile(value, 0.75),
-#     q975 = quantile(value, 0.975)
-#   ) %>% ungroup() %>%
-#   left_join(y %>% rename(Y = value))
-# 
-# g1 <- ggplot(data = fitY) +
-#   geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
-#   geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
-#   geom_line(aes(x = time, y = q50)) +
-#   geom_point(aes(x = time, y = Y), colour = "Red") +
-#   ylab("Daily new H1N1 clinical cases") +
-#   xlab("Time-Day")
-# 
-# plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
-#   group_by(time) %>%
-#   mutate(
-#     q025 = quantile(value, 0.025),
-#     q25 = quantile(value, 0.25),
-#     q50 = quantile(value, 0.5),
-#     q75 = quantile(value, 0.75),
-#     q975 = quantile(value, 0.975)
-#   ) %>% ungroup()
-# 
-# g2 <- ggplot(data = plot_df) +
-#   geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
+#    group_by(time) %>%
+#    mutate(
+#      q025 = quantile(value, 0.025),
+#      q25 = quantile(value, 0.25),
+#      q50 = quantile(value, 0.5),
+#      q75 = quantile(value, 0.75),
+#      q975 = quantile(value, 0.975)
+#    ) %>% ungroup() %>%
+#    left_join(y %>% rename(Y = value))
+#  
+#  g1 <- ggplot(data = fitY) +
+#    geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
+#    geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
+#    geom_line(aes(x = time, y = q50)) +
+#    geom_point(aes(x = time, y = Y), colour = "Red") +
+#    ylab("Daily new H1N1 clinical cases") +
+#    xlab("Time-Day")
+#  
+#  plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
+#    group_by(time) %>%
+#    mutate(
+#      q025 = quantile(value, 0.025),
+#      q25 = quantile(value, 0.25),
+#      q50 = quantile(value, 0.5),
+#      q75 = quantile(value, 0.75),
+#      q975 = quantile(value, 0.975)
+#    ) %>% ungroup()
+#  
+#  g2 <- ggplot(data = plot_df) +
+#    geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
 #   geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
 #   geom_line(aes(x = time, y = q50)) +
 #   ylab(TeX("Transmissibility ($\\beta(t)$)")) +
 #   xlab("Time-Day")
 # 
-# plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>% 
+# plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
 #   group_by(np) %>% mutate(value = value - value[1]) %>%
 #   group_by(time) %>%
 #   mutate(
