949b2f8a704f8fb77178b10dde0f96f2c00cb619
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2022-10-13 18:48:13 +0100

    updatecv1

--- ourdata.R
+++ ourdata.R
@@ -1,14 +1,18 @@
 rm(list=ls())
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+library(pander)
+library(lubridate)
+library(latex2exp)
 library(rbi)
 library(rbi.helpers)
-
 # Load the data
 v <- read.csv("andre_estimates_21_02.txt", sep  = "\t") %>%
   rowSums()
 y <- data.frame(value = v) %>%
   mutate(time = seq(7, by = 7, length.out = n())) %>%
   dplyr::select(time, value)
-#y<- y[1:37,]
 ncores <- 8
 minParticles <- max(ncores, 16)
 
@@ -60,7 +64,7 @@ model dureau {
       dx/dt = sigma*e
       dS/dt = -exp(x)*S*(0.1*I+E)/N
       dE/dt = exp(x)*S*(0.1*I+E)/N - E*(1/k+1/gamma)
-      dI/dt = E/k-I*(1/gamma+0.0087)
+      dI/dt = E/k-I*(1/gamma+0.00026)
       dR/dt = (I+E)/gamma
     }
   }
@@ -89,10 +93,67 @@ bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, ns
   adapt_particles(min = minParticles, max = minParticles*200) %>%
   adapt_proposal(min = 0.05, max = 0.4) %>%
   sample(nsamples = 5000, thin = 5) %>% # burn in 
-  sample(nsamples = 5000, thin = 5)
+  sample(nsamples = 10000, thin = 5)
 
 bi_lst <- bi_read(bi %>% sample_obs)
-write.csv(bi_lst,"ourmodeltheirdataresult.csv")
-par(mfrow=c(2,1))
-g1
-g2
+
+write.csv(bi_lst,"cv37h1n1.csv")
+fitY <- bi_lst$y %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup() %>%
+  left_join(y %>% rename(Y = value))
+write.csv(fitY,"cv37h1n1y.csv")
+#  g1 <- ggplot(data = fitY) +
+#    geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
+#    geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
+#    geom_line(aes(x = time, y = q50)) +
+#    geom_point(aes(x = time, y = Y), colour = "Red") +
+#    ylab("Daily new H1N1 clinical cases") +
+#    xlab("Time-Day")
+#  
+plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup()
+write.csv(plot_df,"cv37h1n1beta.csv")
+#  g2 <- ggplot(data = plot_df) +
+#    geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
+#   geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
+#   geom_line(aes(x = time, y = q50)) +
+#   ylab(TeX("Transmissibility ($\\beta(t)$)")) +
+#   xlab("Time-Day")
+# 
+plot_df1 <- bi_lst$x %>% mutate(value = exp(value)) %>%
+  group_by(np) %>% mutate(value = value - value[1]) %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup()
+write.csv(plot_df1,"cv37h1n1beta1.csv")
+# g3 <- ggplot(data = plot_df) +
+#   geom_ribbon(aes(x = time, ymin = q25, ymax = q75), alpha = 0.3) +
+#   geom_ribbon(aes(x = time, ymin = q025, ymax = q975), alpha = 0.3) +
+#   geom_line(aes(x = time, y = q50)) +
+#   ylab(TeX("Relative trans. ($\\beta(t)-\\beta(0)$)")) +
+#   xlab("Time-Day")
+# 
+# 
+# ggarrange(g1, g2, g3, ncol = 1, nrow = 3, align = "v")
+write.csv(1/bi_lst$k$value,"cv37h1n1alpha.csv")
+write.csv(1/bi_lst$gamma$value,"cv37h1n1gamma.csv")
+
