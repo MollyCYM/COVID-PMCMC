777aeb0c983b8ac0940295cde63abdf29c01f304
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-03-01 22:47:52 +0000

    Exp1_1

--- simulate4.R
+++ simulate4.R
@@ -1,12 +1,14 @@
 rm(list=ls())
 require(deSolve)
-times <- 1:259
+set.seed(007) #015
+times <- 1:365
 N=52196381
-sigma <- runif(1, min = 0, max = 1)
+sigma <- 0.4
 ## first, simulate a set of random deviates
 e <- rnorm(n = length(times) - 1, sd = sqrt(0.01))
 ## now compute their cumulative sum
 e <- c(0, cumsum(e))
+
 
 beta<- exp(sigma*e)
 write.csv(beta,"simulatebeta.csv")
@@ -64,19 +66,19 @@ model1 <- ode(initial_state, times, H1N1, params)
 
 summary(model1)
 
-matplot(model1, type="l", lty=1, main="SEIR model", xlab="Time")
+matplot(model1, type="l", lty=1, main="H1N1-BM SEIR model", xlab="Time",ylab = "Counts")
 legend <- colnames(model1)[3:6]
 legend("right", legend=legend, col=3:6, lty = 1)
 
 Z1 <-model1[,4]/1.59
 
 tau1 <- 0.8#runif(1,0,1)
-Y1 <-vector(length = 259)
-for (i in 1:259){
+Y1 <-vector(length = 365)
+for (i in 1:365){
   #Y1[i]<- rlnorm(1,log(Z1[i,]/5),tau1)
   Y1[i]<- rlnorm(1,log(Z1[i]/5),tau1)
 }
-plot(Y1,type='l')
+plot(Y1,type='l',ylab="Y obs", xlab = "Time")
 write.csv(Y1,"simY11.csv")
 write.csv(model1,"simulatestates1.csv")
 
