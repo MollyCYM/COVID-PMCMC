dc2af9bcbc09a556ff29ad99261d02063010e0f1
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-03-02 14:25:39 +0000

    Exp1_4

--- Exp2_1.R
+++ Exp2_1.R
@@ -8,35 +8,48 @@ library(latex2exp)
 library(rbi)
 library(rbi.helpers)
 # Load the data
-v <- read.csv("H1N1BM_daily1.csv", header=FALSE, stringsAsFactors=FALSE) 
+v <- read.csv("H1N1BM_wk.csv", header=FALSE, stringsAsFactors=FALSE) %>%
+  rowSums()
+
 y <- data.frame(value = v) %>%
+  mutate(time = seq(7, by = 7, length.out = n())) %>%
+  dplyr::select(time, value)
+L <- read.csv("Forcing.csv", header=FALSE, stringsAsFactors=FALSE)
+Forcing <- data.frame(value = L) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
-  dplyr::select(time, V1)
-colnames(y) <- c("time", "value")
+  dplyr::select(time,V1 )
+colnames(Forcing) <- c("time","value")
+
 ncores <- 8
 minParticles <- max(ncores, 16)
 model_str <- "
 model dureau {
   obs y
-
+  
+  const theta=0
   state S
   state E
   state I
   state R
+  state mu
   state x
 
   state Z
 
   input N
+  input Forcing
+  
   param k
   param gamma
   param sigma // Noise driver
+  param a
+  param b
   param E0
   param I0
   param R0
   param x0
   param tau
-
+  
   sub parameter {
     k ~ truncated_gaussian(1.59, 0.02, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
     gamma ~ truncated_gaussian(1.08, 0.075, lower = 0) // gamma is the period, not the rate
@@ -46,6 +59,8 @@ model dureau {
     E0 ~ uniform(-16, -9)
     R0 ~ truncated_gaussian(0.15, 0.15, lower = 0, upper = 1)
     tau ~ uniform(0, 1)
+    a ~ gaussian(-0.02, 0.01)
+    b ~ gaussian(-0.2, 0.01)
   }
 
   sub initial {
@@ -62,10 +77,12 @@ model dureau {
   }
 
   sub transition(delta = 1) {
+  Z <- ((t_now) % 7 == 0 ? 0 : Z)
     noise e
     e ~ wiener()
+    mu <- a+b*Forcing
     ode(alg = 'RK4(3)', h = 1.0, atoler = 1.0e-3, rtoler = 1.0e-8) {
-      dx/dt = sigma*e
+      dx/dt = theta*(mu-x)+sigma*e
       dS/dt = -exp(x)*S*I/N
       dE/dt = exp(x)*S*I/N - E/k
       dI/dt = E/k-I/gamma
@@ -87,6 +104,8 @@ model dureau {
     I0 ~ gaussian(I0, 0.05)
     R0 ~ gaussian(R0, 0.05)
     tau ~ gaussian(tau, 0.05)
+    a ~ gaussian(a, 0.01)
+    b ~ gaussian(b, 0.01)
   }
 }"
 model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
@@ -95,7 +114,7 @@ input_lst <- list(N = 52196381)
 end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
 
-bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, nsamples = 1000, nparticles = minParticles, nthreads = ncores, proposal = 'prior',seed=0123) %>% 
+bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, nsamples = 1000, nparticles = minParticles, nthreads = ncores, proposal = 'prior',seed=0121) %>% 
   adapt_particles(min = minParticles, max = minParticles*200) %>%
   adapt_proposal(min = 0.05, max = 0.4) %>%
   sample(nsamples = 1000, thin = 1) %>% # burn in 
@@ -103,7 +122,7 @@ bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, ns
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/Exp1_model4.csv")
+write.csv(bi_lst,"../data/Exp2_model1.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -114,7 +133,7 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/Exp1_y4.csv")
+write.csv(fitY,"../data/Exp2_y1.csv")
 
 plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(time) %>%
@@ -125,7 +144,7 @@ plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/Exp1_beta4.csv")
+write.csv(plot_df,"../data/Exp2_beta1.csv")
 
 plot_df1 <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(np) %>% mutate(value = value - value[1]) %>%
@@ -137,9 +156,20 @@ plot_df1 <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df1,"../data/Exp1_beta04.csv")
+write.csv(plot_df1,"../data/Exp2_beta01.csv")
 
-Mmodel <- read.csv("simulatestates1_2.csv", header=TRUE, stringsAsFactors=FALSE)
+fitmu <-bi_lst$mu %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) 
+write.csv(fitmu,"../data/Exp2_mu1.csv")
+
+Mmodel <- read.csv("simulatestates1.csv", header=TRUE, stringsAsFactors=FALSE)
 S<-Mmodel[,4]
 E<-Mmodel[,5]
 I<-Mmodel[,6]
@@ -158,7 +188,7 @@ fitS <-bi_lst$S %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/Exp1_S4.csv")
+write.csv(fitS,"../data/Exp2_S1.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -173,7 +203,7 @@ fitE <-bi_lst$E %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/Exp1_E4.csv")
+write.csv(fitE,"../data/Exp2_E1.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -188,7 +218,7 @@ fitI <-bi_lst$I %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/Exp1_I4.csv")
+write.csv(fitI,"../data/Exp2_I1.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -203,9 +233,8 @@ fitR <-bi_lst$R %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/Exp1_R4.csv")
+write.csv(fitR,"../data/Exp2_R1.csv")
 
-
-write.csv(1/bi_lst$k$value,"../data/Exp1_alpha4.csv")
-write.csv(1/bi_lst$gamma$value,"../data/Exp1_gamma4.csv")
+write.csv(1/bi_lst$k$value,"../data/Exp2_alpha1.csv")
+write.csv(1/bi_lst$gamma$value,"../data/Exp2_gamma1.csv")
 
