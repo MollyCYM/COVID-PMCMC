8863a53a25adecfe3732673731c89dd5890fccd7
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-03-06 11:17:25 +0000

    poster

--- papercode.R
+++ papercode.R
@@ -47,7 +47,6 @@ model dureau {
   param E0
   param I0
   param R0
-  param x0
   param tau
   
   sub parameter {
@@ -55,7 +54,6 @@ model dureau {
     gamma ~ truncated_gaussian(5, 0.1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.0001, lower = 0)
     theta ~ truncated_gaussian(0.05, 0.001, lower = 0)
-    x0 ~ uniform(-5,2)
     I0 ~ uniform(-16, -9)
     E0 ~ uniform(-16, -9)
     R0 ~ truncated_gaussian(0.15, 0.15, lower = 0, upper = 1)
@@ -73,7 +71,7 @@ model dureau {
     S <- S - E
     I <- exp(I0 + log(S))
     S <- S - I
-    x <- x0
+    x <- log(0.8)
     Z <- 0
   }
 
@@ -101,7 +99,6 @@ model dureau {
     gamma ~ truncated_gaussian(5, 0.1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.0001, lower = 0)
     theta ~ truncated_gaussian(0.05, 0.001, lower = 0)
-    x0 ~ gaussian(x0, 0.05)
     E0 ~ gaussian(E0, 0.05)
     I0 ~ gaussian(I0, 0.05)
     R0 ~ gaussian(R0, 0.05)
@@ -124,7 +121,7 @@ bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, ns
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/postermodel.csv")
+write.csv(bi_lst,"../data/postermodel1.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -135,7 +132,7 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/poster_y.csv")
+write.csv(fitY,"../data/poster_y1.csv")
 
 plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(time) %>%
@@ -146,7 +143,7 @@ plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/poster_beta.csv")
+write.csv(plot_df,"../data/poster_beta1.csv")
 
 plot_df1 <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(np) %>% mutate(value = value - value[1]) %>%
@@ -158,7 +155,7 @@ plot_df1 <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df1,"../data/poster_beta0.csv")
+write.csv(plot_df1,"../data/poster_beta01.csv")
 
 fitmu <-bi_lst$mu %>%
   group_by(time) %>%
@@ -169,7 +166,7 @@ fitmu <-bi_lst$mu %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) 
-write.csv(fitmu,"../data/poster_mu.csv")
+write.csv(fitmu,"../data/poster_mu1.csv")
 
 Mmodel <- read.csv("Covidou1.csv", header=TRUE, stringsAsFactors=FALSE)
 S<-Mmodel[,4]
@@ -190,7 +187,7 @@ fitS <-bi_lst$S %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/poster_S.csv")
+write.csv(fitS,"../data/poster_S1.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -205,7 +202,7 @@ fitE <-bi_lst$E %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/poster_E.csv")
+write.csv(fitE,"../data/poster_E1.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -220,7 +217,7 @@ fitI <-bi_lst$I %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/poster_I.csv")
+write.csv(fitI,"../data/poster_I1.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -235,11 +232,11 @@ fitR <-bi_lst$R %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/poster_R.csv")
+write.csv(fitR,"../data/poster_R1.csv")
 
-write.csv(1/bi_lst$k$value,"../data/poster_alpha.csv")
-write.csv(1/bi_lst$gamma$value,"../data/poster_gamma.csv")
-write.csv(bi_lst$sigma$value,"../data/poster_sigma.csv")
-write.csv(bi_lst$theta$value,"../data/poster_theta.csv")
+write.csv(1/bi_lst$k$value,"../data/poster_alpha1.csv")
+write.csv(1/bi_lst$gamma$value,"../data/poster_gamma1.csv")
+write.csv(bi_lst$sigma$value,"../data/poster_sigma1.csv")
+write.csv(bi_lst$theta$value,"../data/poster_theta1.csv")
 
 
