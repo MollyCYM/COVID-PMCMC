9e0027c647b4d89d7116ba264f0679444e10d3a9
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-04-15 01:19:07 +0100

    para21_4

--- para21_4.R
+++ para21_4.R
@@ -106,7 +106,7 @@ bi <- sample(bi_model, end_time = end_time, input = input_lst, init=init_list, o
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/para21_model3.csv")
+write.csv(bi_lst,"../data/para21_model4.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -117,7 +117,7 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/para21_y3.csv")
+write.csv(fitY,"../data/para21_y4.csv")
 
 plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(time) %>%
@@ -128,7 +128,7 @@ plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/para21_beta3.csv")
+write.csv(plot_df,"../data/para21_beta4.csv")
 
 Mmodel <- read.csv("Covidou1.csv", header=TRUE, stringsAsFactors=FALSE)
 S<-Mmodel[,4]
@@ -149,7 +149,7 @@ fitS <-bi_lst$S %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/para21_S3.csv")
+write.csv(fitS,"../data/para21_S4.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -164,7 +164,7 @@ fitE <-bi_lst$E %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/para21_E3.csv")
+write.csv(fitE,"../data/para21_E4.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -179,7 +179,7 @@ fitI <-bi_lst$I %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/para21_I3.csv")
+write.csv(fitI,"../data/para21_I4.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -194,8 +194,8 @@ fitR <-bi_lst$R %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/para21_R3.csv")
+write.csv(fitR,"../data/para21_R4.csv")
 
-write.csv(1/bi_lst$k$value,"../data/para21_alpha3.csv")
+write.csv(1/bi_lst$k$value,"../data/para21_alpha4.csv")
 
 
