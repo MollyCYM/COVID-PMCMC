a1e49697b47eca9190aa6b1d7e92c860712e1d1f
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-04-18 23:59:30 +0100

    para1_3

---
+++ para1_3.R
@@ -0,0 +1,179 @@
+rm(list=ls())
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+library(pander)
+library(lubridate)
+library(latex2exp)
+library(rbi)
+library(rbi.helpers)
+# Load the data
+v <- read.csv("covidbm_wk2.csv", header=FALSE, stringsAsFactors=FALSE) %>%
+  rowSums()
+
+y <- data.frame(value = v) %>%
+  mutate(time = seq(7, by = 7, length.out = n())) %>%
+  dplyr::select(time, value)
+
+ncores <- 8
+minParticles <- max(ncores, 16)
+model_str <- "
+model dureau {
+  obs y
+
+  state S
+  state E
+  state I
+  state R
+  state x
+  state Z
+
+  input N
+  
+  param k
+  param E0
+  
+  sub parameter {
+    k ~ truncated_gaussian(5, 0.01, lower = 0) 
+    E0 ~ truncated_gaussian(1, 0.001, lower = 0) 
+  }
+
+  sub initial {
+    S <- N-E0
+    E <- E0
+    I <- 0
+    R <- 0
+    Z <- 0
+  }
+
+  sub transition(delta = 1) {
+  Z <- ((t_now) % 7 == 0 ? 0 : Z)
+    noise e
+    e ~ wiener()
+    ode(alg = 'RK4(3)', h = 1.0, atoler = 1.0e-3, rtoler = 1.0e-8) {
+      dx/dt = 0.4*e
+      dS/dt = -exp(x)*S*(0.1*I+E)/N
+      dE/dt = exp(x)*S*(0.1*I+E)/N - E*(1/k+1/5)
+      dI/dt = E/k-I*(1/5+0.0087)
+      dR/dt = (I+E)/5+0.0087*I
+      dZ/dt = E/k
+    }
+  }
+
+  sub observation {
+    y ~ log_normal(log(max(Z/5, 0)), 0.8)
+  }
+  
+  sub proposal_parameter {
+    k ~ truncated_gaussian(k, 0.0001, lower = 0) 
+    E0 ~ gaussian(E0, 0.0001)
+  }
+}"
+model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
+bi_model <- libbi(model)
+input_lst <- list(N = 52196381)
+end_time <- max(y$time)
+obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
+init_list <- list(k=5)
+
+bi <- sample(bi_model, end_time = end_time, input = input_lst, init=init_list, obs = obs_lst, nsamples = 1000, nparticles = minParticles, nthreads = ncores, filter= 'lookahead', proposal = 'model',seed=0123112) %>% 
+  adapt_particles(min = minParticles, max = minParticles*500) %>%
+  adapt_proposal(min = 0.1, max = 0.4) %>%
+  sample(nsamples = 1, thin = 1) %>% # burn in 
+  sample(nsamples = 5000, thin = 5)
+
+bi_lst <- bi_read(bi %>% sample_obs)
+
+write.csv(bi_lst,"../data/para1_model3.csv")
+fitY <- bi_lst$y %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup() %>%
+  left_join(y %>% rename(Y = value))
+write.csv(fitY,"../data/para1_y3.csv")
+
+plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup()
+write.csv(plot_df,"../data/para1_beta3.csv")
+
+Mmodel <- read.csv("Covidbm12.csv", header=TRUE, stringsAsFactors=FALSE)
+S<-Mmodel[,4]
+E<-Mmodel[,5]
+I<-Mmodel[,6]
+R<-Mmodel[,7]
+
+S <- data.frame(value = S) %>%
+  mutate(time = seq(1, by = 1, length.out = n())) %>%
+  dplyr::select(time, value)
+fitS <-bi_lst$S %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup() %>%
+  left_join(S %>% rename(S = value))
+write.csv(fitS,"../data/para1_S3.csv")
+
+E <- data.frame(value = E) %>%
+  mutate(time = seq(1, by = 1, length.out = n())) %>%
+  dplyr::select(time, value)
+fitE <-bi_lst$E %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup() %>%
+  left_join(E %>% rename(E = value))
+write.csv(fitE,"../data/para1_E3.csv")
+
+I <- data.frame(value = I) %>%
+  mutate(time = seq(1, by = 1, length.out = n())) %>%
+  dplyr::select(time, value)
+fitI <-bi_lst$I %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup() %>%
+  left_join(I %>% rename(I = value))
+write.csv(fitI,"../data/para1_I3.csv")
+
+R <- data.frame(value = R) %>%
+  mutate(time = seq(1, by = 1, length.out = n())) %>%
+  dplyr::select(time, value)
+fitR <-bi_lst$R %>%
+  group_by(time) %>%
+  mutate(
+    q025 = quantile(value, 0.025),
+    q25 = quantile(value, 0.25),
+    q50 = quantile(value, 0.5),
+    q75 = quantile(value, 0.75),
+    q975 = quantile(value, 0.975)
+  ) %>% ungroup() %>%
+  left_join(R %>% rename(R = value))
+write.csv(fitR,"../data/para1_R3.csv")
+
+write.csv(1/bi_lst$k$value,"../data/para1_alpha3.csv")
+
+
