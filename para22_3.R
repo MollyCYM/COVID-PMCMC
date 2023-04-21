c8da6bcb0405cb00628e8b6c490b8e62bc8a732a
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-04-21 11:03:34 +0100

    para3_18

--- para22_3.R
+++ para22_3.R
@@ -8,49 +8,60 @@ library(latex2exp)
 library(rbi)
 library(rbi.helpers)
 # Load the data
-v <- read.csv("H1N1BM_wk.csv", header=FALSE, stringsAsFactors=FALSE) %>%
+v <- read.csv("covidou_wk2.csv", header=FALSE, stringsAsFactors=FALSE) %>%
   rowSums()
 
 y <- data.frame(value = v) %>%
   mutate(time = seq(7, by = 7, length.out = n())) %>%
   dplyr::select(time, value)
+L <- read.csv("Forcing.csv", header=FALSE, stringsAsFactors=FALSE)
+Forcing <- data.frame(value = L) %>%
+  mutate(time = seq(1, by = 1, length.out = n())) %>%
+  dplyr::select(time,V1 )
+colnames(Forcing) <- c("time","value")
+
 ncores <- 8
 minParticles <- max(ncores, 16)
 model_str <- "
-model dureau {
+model covid {
   obs y
-
+  
   state S
   state E
   state I
   state R
+  state mu
   state x
 
   state Z
 
   input N
+  input Forcing
+  
   param k
   param gamma
   param sigma // Noise driver
-  param x0
-
+  param theta
+  param a
+  param b
+  param tau
+  
   sub parameter {
-    k ~ truncated_gaussian(1.59, 0.02, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
-    gamma ~ truncated_gaussian(1.08, 0.075, lower = 0) // gamma is the period, not the rate
-    sigma ~ truncated_gaussian(0.05, 0.002, lower = 0)
-    x0 ~ uniform(-5,2)
+    k ~ truncated_gaussian(5, 0.01, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
+    gamma ~ truncated_gaussian(5, 0.01, lower = 0) // gamma is the period, not the rate
+    sigma ~ truncated_gaussian(sqrt(0.004), 0.00001, lower = 0)
+    theta ~ truncated_gaussian(0.05, 0.0001, lower = 0)
+    tau ~ uniform(0, 1)
+    a ~ gaussian(-0.02, 0.01)
+    b ~ gaussian(-0.2, 0.01)
   }
 
   sub initial {
-    S <- N
-    R <- 0.2565*S
-    S <- S - R
-
-    E <- exp(-14.0622 + log(S))
-    S <- S - E
-    I <- exp(-15.1255 + log(S))
-    S <- S - I
-    x <- x0
+    S <- N-1
+    E <- 1
+    I <- 0
+    R <- 0
+    x <- log(0.8)
     Z <- 0
   }
 
@@ -58,25 +69,29 @@ model dureau {
   Z <- ((t_now) % 7 == 0 ? 0 : Z)
     noise e
     e ~ wiener()
+    mu <- a+b*Forcing
     ode(alg = 'RK4(3)', h = 1.0, atoler = 1.0e-3, rtoler = 1.0e-8) {
-      dx/dt = sigma*e
-      dS/dt = -exp(x)*S*I/N
-      dE/dt = exp(x)*S*I/N - E/k
-      dI/dt = E/k-I/gamma
-      dR/dt = I/gamma
+      dx/dt = theta*(mu-x)+sigma*e
+      dS/dt = -exp(x)*S*(0.1*I+E)/N
+      dE/dt = exp(x)*S*(0.1*I+E)/N - E*(1/k+1/gamma)
+      dI/dt = E/k-I*(1/gamma+0.0087)
+      dR/dt = (I+E)/gamma+0.0087*I
       dZ/dt = E/k
     }
   }
 
   sub observation {
-    y ~ log_normal(log(max(Z/5, 0)), 0.8)
+    y ~ log_normal(log(max(Z/5, 0)), tau)
   }
 
   sub proposal_parameter {
-    k ~ gaussian(k, 0.005)
-    sigma ~ gaussian(sigma, 0.001)
-    gamma ~ gaussian(gamma, 0.01)
-    x0 ~ gaussian(x0, 0.05)
+    k ~ truncated_gaussian(k, 0.001, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.001, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
+    theta ~ truncated_gaussian(theta, 0.001, lower = 0)
+    tau ~ gaussian(tau, 0.05)
+    a ~ gaussian(a, 0.01)
+    b ~ gaussian(b, 0.01)
   }
 }"
 model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
@@ -84,17 +99,17 @@ bi_model <- libbi(model)
 input_lst <- list(N = 52196381)
 end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
-init_list <- list(sigma=0.4, gamma=1.08, k=1.59)
+init_list <- list(k=3,gamma=3,sigma=sqrt(0.003),theta=0.04,a=-0.02,b=-0.2)
 
-bi <- sample(bi_model, end_time = end_time, input = input_lst,init=init_list, obs = obs_lst, nsamples = 1000, nparticles = minParticles, nthreads = ncores, proposal = 'model',seed=123) %>% 
-  adapt_particles(min = minParticles, max = minParticles*200) %>%
-  adapt_proposal(min = 0.05, max = 0.4) %>%
-  sample(nsamples = 1000, thin = 1) %>% # burn in 
-  sample(nsamples = 4000, thin = 5)
+bi <- sample(bi_model, end_time = end_time, input = input_lst, init=init_list, obs = obs_lst, nsamples = 1000, nparticles = minParticles, nthreads = ncores, proposal = 'model',seed=111123) %>% 
+  adapt_particles(min = minParticles, max = minParticles*500) %>%
+  adapt_proposal(min = 0.1, max = 0.4) %>%
+  sample(nsamples = 1, thin = 1) %>% # burn in 
+  sample(nsamples = 5000, thin = 5)
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/para3_model18.csv")
+write.csv(bi_lst,"../data/para22_model3.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -105,7 +120,7 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/para3_y18.csv")
+write.csv(fitY,"../data/para22_y3.csv")
 
 plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(time) %>%
@@ -116,9 +131,9 @@ plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/para3_beta18.csv")
+write.csv(plot_df,"../data/para22_beta3.csv")
 
-Mmodel <- read.csv("simulatestates1.csv", header=TRUE, stringsAsFactors=FALSE)
+Mmodel <- read.csv("Covidou5.csv", header=TRUE, stringsAsFactors=FALSE)
 S<-Mmodel[,4]
 E<-Mmodel[,5]
 I<-Mmodel[,6]
@@ -137,7 +152,7 @@ fitS <-bi_lst$S %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/para3_S18.csv")
+write.csv(fitS,"../data/para22_S3.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -152,7 +167,7 @@ fitE <-bi_lst$E %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/para3_E18.csv")
+write.csv(fitE,"../data/para22_E3.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -167,7 +182,7 @@ fitI <-bi_lst$I %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/para3_I18.csv")
+write.csv(fitI,"../data/para22_I3.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -182,10 +197,13 @@ fitR <-bi_lst$R %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/para3_R18.csv")
+write.csv(fitR,"../data/para22_R3.csv")
 
-
-write.csv(1/bi_lst$k$value,"../data/para3_alpha18.csv")
-write.csv(1/bi_lst$gamma$value,"../data/para3_gamma18.csv")
-write.csv(bi_lst$sigma$value,"../data/para3_sigma18.csv")
+write.csv(1/bi_lst$k$value,"../data/para22_alpha3.csv")
+write.csv(1/bi_lst$gamma$value,"../data/para22_gamma3.csv")
+write.csv(bi_lst$sigma$value,"../data/para22_sigma3.csv")
+write.csv(bi_lst$theta$value,"../data/para22_theta3.csv")
+write.csv(bi_lst$tau$value,"../data/para22_tau3.csv")
+write.csv(bi_lst$a$value,"../data/para22_a3.csv")
+write.csv(bi_lst$b$value,"../data/para22_b3.csv")
 
