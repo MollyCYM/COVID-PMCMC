3a76986a7b17ed256652d28be1cc2b836685820e
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-04-23 23:25:55 +0100

    para3_10

--- para3_10.R
+++ para3_10.R
@@ -100,7 +100,8 @@ obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
 bi <- sample(bi_model, end_time = end_time, input = input_lst, obs = obs_lst, nsamples = 1, nparticles = minParticles, nthreads = ncores, proposal = 'prior',seed=123) %>% 
   adapt_particles(min = minParticles, max = minParticles*200) %>%
   adapt_proposal(min = 0.05, max = 0.4) %>%
-  sample(nsamples = 5000, thin = 1)
+  sample(nsamples = 1, thin = 1) %>% # burn in 
+  sample(nsamples =5000, thin = 5)
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
