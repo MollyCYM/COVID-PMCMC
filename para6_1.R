de76c8b3eb7313630b6a6f4f21cf885d3db6c221
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-07-11 20:30:09 +0100

    reruncovidou

--- para6_1.R
+++ para6_1.R
@@ -101,102 +101,15 @@ end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
 init_list <- list(k=5, gamma=9, sigma=sqrt(0.004),theta=0.05,tau=0.1,a=-0.02,b=-0.2)
 
-bi <- sample(bi_model,target = "posterior", end_time = end_time, input = input_lst, init=init_list, obs = obs_lst, nsamples = 2000, nparticles = minParticles, nthreads = ncores, proposal = 'model',seed=0066661) %>% 
+bi <- sample(bi_model,target = "posterior", end_time = end_time, input = input_lst, init=init_list,
+             obs = obs_lst, nsamples = 1000,nparticles = minParticles,
+             nthreads = ncores, proposal = 'model',seed=0066661) %>% 
   adapt_particles(min = minParticles, max = minParticles*500) %>%
   adapt_proposal(min = 0.1, max = 0.4)
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
 write.csv(bi_lst,"../data/para6_model1.csv")
-fitY <- bi_lst$y %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup() %>%
-  left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/para6_y1.csv")
-
-plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup()
-write.csv(plot_df,"../data/para6_beta1.csv")
-
-Mmodel <- read.csv("covidoudg_model1.csv", header=TRUE, stringsAsFactors=FALSE)
-S<-Mmodel[,4]
-E<-Mmodel[,5]
-I<-Mmodel[,6]
-R<-Mmodel[,7]
-
-S <- data.frame(value = S) %>%
-  mutate(time = seq(1, by = 1, length.out = n())) %>%
-  dplyr::select(time, value)
-fitS <-bi_lst$S %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup() %>%
-  left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/para6_S1.csv")
-
-E <- data.frame(value = E) %>%
-  mutate(time = seq(1, by = 1, length.out = n())) %>%
-  dplyr::select(time, value)
-fitE <-bi_lst$E %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup() %>%
-  left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/para6_E1.csv")
-
-I <- data.frame(value = I) %>%
-  mutate(time = seq(1, by = 1, length.out = n())) %>%
-  dplyr::select(time, value)
-fitI <-bi_lst$I %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup() %>%
-  left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/para6_I1.csv")
-
-R <- data.frame(value = R) %>%
-  mutate(time = seq(1, by = 1, length.out = n())) %>%
-  dplyr::select(time, value)
-fitR <-bi_lst$R %>%
-  group_by(time) %>%
-  mutate(
-    q025 = quantile(value, 0.025),
-    q25 = quantile(value, 0.25),
-    q50 = quantile(value, 0.5),
-    q75 = quantile(value, 0.75),
-    q975 = quantile(value, 0.975)
-  ) %>% ungroup() %>%
-  left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/para6_R1.csv")
-
 
 write.csv(1/bi_lst$k$value,"../data/para6_alpha1.csv")
 write.csv(1/bi_lst$gamma$value,"../data/para6_gamma1.csv")
