fc7cdb5eea8a3aebff6d0582cd965a0fe04cd58e
Molly Cui <mollycui@Mollys-MacBook-Pro-2.local>
2023-07-19 12:20:55 +0100

    reruntest6_67

--- para6_31.R
+++ para6_31.R
@@ -8,7 +8,7 @@ library(latex2exp)
 library(rbi)
 library(rbi.helpers)
 # Load the data
-v <- read.csv("covidoudg2_y1w.csv", header=FALSE, stringsAsFactors=FALSE) %>%
+v <- read.csv("covidoudg2_y4w.csv", header=FALSE, stringsAsFactors=FALSE) %>%
   rowSums()
 
 y <- data.frame(value = v) %>%
@@ -37,19 +37,24 @@ model dureau {
 
   input N
   input Forcing
-
-  const k = 5
-  const sigma = sqrt(0.004)
-  const theta = 0.05
-  const a = -0.02
-  const b = -0.2
-  const tau = 0.1
   
+  const sigma = 0.000001
+  
+  param k
   param gamma
+  param theta
+  param a
+  param b
+  param tau
   param x0
   
   sub parameter {
-    gamma ~ truncated_gaussian(9, 1, lower = 0) 
+    k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
+    gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
+    theta ~ truncated_gaussian(0.05, 0.1, lower = 0)
+    tau ~ truncated_gaussian(0.1, 0.05, lower = 0)
+    a ~ truncated_gaussian(-0.02, 0.05, upper = 0)
+    b ~ truncated_gaussian(-0.2, 0.1, upper = 0)
     x0 ~ gaussian(-0.02, 0.2) //long-term ou mean & sd
   }
 
@@ -82,7 +87,12 @@ model dureau {
   }
 
   sub proposal_parameter {
+    k ~ truncated_gaussian(k, 0.01, lower = 0) 
     gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
+    theta ~ truncated_gaussian(theta, 0.001, lower = 0)
+    tau ~ gaussian(tau, 0.001)
+    a ~ gaussian(a, 0.001)
+    b ~ gaussian(b, 0.001)
   }
 }"
 model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
@@ -90,15 +100,16 @@ bi_model <- libbi(model)
 input_lst <- list(N = 52196381,Forcing=Forcing)
 end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
+init_list <- list(k=5, gamma=9,theta=0.05,tau=0.1,a=-0.02,b=-0.2)
 
-bi <- sample(bi_model,target = "posterior", end_time = end_time, input = input_lst,obs = obs_lst, nsamples = 2000, nparticles = minParticles, nthreads = ncores, proposal = 'model',seed=0066661) %>% 
+bi <- sample(bi_model,target = "posterior", end_time = end_time, input = input_lst, init=init_list, obs = obs_lst, nsamples = 2000, nparticles = minParticles, nthreads = ncores, proposal = 'model',seed=0066661) %>% 
   adapt_particles(min = minParticles, max = minParticles*500) %>%
   adapt_proposal(min = 0.1, max = 0.4) %>%
   sample(nsamples = 10000, thin = 1)
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/para6_model7.csv")
+write.csv(bi_lst,"../data/para6_model31.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -109,7 +120,7 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/para6_y7.csv")
+write.csv(fitY,"../data/para6_y31.csv")
 
 plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(time) %>%
@@ -120,9 +131,9 @@ plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/para6_beta7.csv")
+write.csv(plot_df,"../data/para6_beta31.csv")
 
-Mmodel <- read.csv("covidoudg2_model1.csv", header=TRUE, stringsAsFactors=FALSE)
+Mmodel <- read.csv("covidoudg2_model4.csv", header=TRUE, stringsAsFactors=FALSE)
 S<-Mmodel[-1,7]
 E<-Mmodel[-1,9]
 I<-Mmodel[-1,11]
@@ -141,7 +152,7 @@ fitS <-bi_lst$S %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/para6_S7.csv")
+write.csv(fitS,"../data/para6_S31.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -156,7 +167,7 @@ fitE <-bi_lst$E %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/para6_E7.csv")
+write.csv(fitE,"../data/para6_E31.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -171,7 +182,7 @@ fitI <-bi_lst$I %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/para6_I7.csv")
+write.csv(fitI,"../data/para6_I31.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -186,10 +197,17 @@ fitR <-bi_lst$R %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/para6_R7.csv")
+write.csv(fitR,"../data/para6_R31.csv")
 
 
-write.csv(1/bi_lst$gamma$value,"../data/para6_gamma7.csv")
+write.csv(1/bi_lst$k$value,"../data/para6_alpha31.csv")
+write.csv(1/bi_lst$gamma$value,"../data/para6_gamma31.csv")
+write.csv(bi_lst$tau$value,"../data/para6_tau31.csv")
+write.csv(bi_lst$theta$value,"../data/para6_theta31.csv")
+write.csv(bi_lst$a$value,"../data/para6_a31.csv")
+write.csv(bi_lst$b$value,"../data/para6_b31.csv")
+
+
 
 
 
