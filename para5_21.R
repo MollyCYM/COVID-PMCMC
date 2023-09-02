eca9c806786d050e030fa5eb4e4285be7d15e0a0
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-09-02 19:49:52 +0100

    benchmark

--- para5_21.R
+++ para5_21.R
@@ -92,15 +92,29 @@ model dureau {
   }
 }"
 model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
-bi_model <- libbi(model)
+
 input_lst <- list(N = 52196381,Forcing=Forcing)
 end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
-init_list <- list(k=5, gamma=9, sigma=sqrt(0.004),theta=0.05,a=-0.02,b=-0.2)
+init_list <- list(k=5, gamma=9, sigma=sqrt(0.004),theta=0.05,tau=0.1,a=-0.02,b=-0.2)
+#LibBi wrapper 
+#run launches LibBi with a particular set of command line arguments
+bi_model <- libbi(model,end_time = end_time, input = input_lst, 
+                  init=init_list, obs = obs_lst)
+#RBi.helpers adapt_particle
+particles_adapted <- bi_model %>%
+  sample(nsamples = 2000, nparticles = minParticles, 
+         nthreads = ncores, proposal = 'prior',seed=0066661) %>%
+  adapt_particles(min = minParticles, max = minParticles*500)
 
-bi <- sample(bi_model,target = "posterior", end_time = end_time, input = input_lst, init=init_list, obs = obs_lst, nsamples = 2000, nparticles = minParticles, nthreads = ncores, proposal = 'model',seed=0066661) %>% 
-  adapt_particles(min = minParticles, max = minParticles*500) %>%
-  adapt_proposal(min = 0.1, max = 0.4) %>%
+#RBi.helpers adapt_proposal
+proposal_adapted <- particles_adapted %>%
+  sample(target = "posterior", nsamples = 2000, 
+         nthreads = ncores, proposal = 'model',seed=0066661) %>%
+  adapt_proposal(min = 0.1, max = 0.4)
+
+#Running pMCMC with burn-in
+bi <- proposal_adapted %>%
   sample(nsamples = 10000, thin = 1, init=init_list)
 
 bi_lst <- bi_read(bi %>% sample_obs)
