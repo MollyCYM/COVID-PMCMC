df10a3829f1d8a48c0b11a8f05c6c809da078aee
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-09-03 13:20:56 +0100

    benchmark

--- para5_213.R
+++ para5_213.R
@@ -49,9 +49,9 @@ model dureau {
     k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
     gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
-    theta ~ truncated_gaussian(0.05, 0.1, lower = 0)
-    a ~ truncated_gaussian(-0.02, 0.05, upper = 0)
-    b ~ truncated_gaussian(-0.2, 0.1, upper = 0)
+    theta ~ truncated_gaussian(0.05, 0.2, lower = 0)
+    a ~ truncated_gaussian(-0.02, 0.1, upper = 0)
+    b ~ truncated_gaussian(-0.2, 0.2, upper = 0)
   }
 
   sub initial {
