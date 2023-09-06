822ae5c50d43488b6bbc1d9f2216eb8b2bd82d46
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-09-06 18:55:28 +0100

    binomial tunning

--- para5_2111.R
+++ para5_2111.R
@@ -8,12 +8,12 @@ library(latex2exp)
 library(rbi)
 library(rbi.helpers)
 # Load the data
-v <- read.csv("covidoudg2_y1w.csv", header=FALSE, stringsAsFactors=FALSE) %>%
+v <- read.csv("covidoudg2_y3w.csv", header=FALSE, stringsAsFactors=FALSE) %>%
   rowSums()
+
 y <- data.frame(value = v) %>%
   mutate(time = seq(7, by = 7, length.out = n())) %>%
   dplyr::select(time, value)
-
 L <- read.csv("Forcing.csv", header=FALSE, stringsAsFactors=FALSE)
 Forcing <- data.frame(value = L) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -44,17 +44,14 @@ model dureau {
   param theta
   param a
   param b
-  param tau
-
   
-  sub parameter {
+   sub parameter {
     k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
     gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
-    theta ~ truncated_gaussian(0.05, 0.1, lower = 0)
-    tau ~ truncated_gaussian(0.1, 0.05, lower = 0)
-    a ~ truncated_gaussian(-0.02, 0.05, upper = 0)
-    b ~ truncated_gaussian(-0.2, 0.1, upper = 0)
+    theta ~ truncated_gaussian(0.05, 0.2, lower = 0)
+    a ~ truncated_gaussian(-0.02, 0.1, upper = 0)
+    b ~ truncated_gaussian(-0.2, 0.2, upper = 0)
   }
 
   sub initial {
@@ -82,26 +79,24 @@ model dureau {
   }
 
   sub observation {
-    y ~ log_normal(log(max(Z/5, 0)), tau)
+    y ~ binomial(floor(Z),1/5)
   }
 
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.01, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
-    theta ~ truncated_gaussian(theta, 0.001, lower = 0)
-    tau ~ gaussian(tau, 0.001)
-    a ~ gaussian(a, 0.001)
-    b ~ gaussian(b, 0.001)
+    k ~ truncated_gaussian(k, 0.001, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.001, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.0001, lower = 0)
+    theta ~ truncated_gaussian(theta, 0.0001, lower = 0)
+    a ~ gaussian(a, 0.0001)
+    b ~ gaussian(b, 0.0001)
   }
 }"
-
 model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
 
 input_lst <- list(N = 52196381,Forcing=Forcing)
 end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
-init_list <- list(k=5, gamma=9, sigma=sqrt(0.004),theta=0.05,tau=0.1,a=-0.02,b=-0.2)
+init_list <- list(k=3, gamma=7, sigma=0.04,theta=0.03,a=-0.01,b=-0.1)
 #LibBi wrapper 
 #run launches LibBi with a particular set of command line arguments
 bi_model <- libbi(model,end_time = end_time, input = input_lst, 
@@ -109,13 +104,13 @@ bi_model <- libbi(model,end_time = end_time, input = input_lst,
 #RBi.helpers adapt_particle
 particles_adapted <- bi_model %>%
   sample(nsamples = 2000, nparticles = minParticles, 
-         nthreads = ncores, proposal = 'prior',seed=0066661) %>%
+         nthreads = ncores, proposal = 'prior',seed=00007788) %>%
   adapt_particles(min = minParticles, max = minParticles*500)
 
 #RBi.helpers adapt_proposal
 proposal_adapted <- particles_adapted %>%
   sample(target = "posterior", nsamples = 2000, 
-         nthreads = ncores, proposal = 'model',seed=0066661) %>%
+         nthreads = ncores, proposal = 'model',seed=00007788) %>%
   adapt_proposal(min = 0.1, max = 0.4)
 
 #Running pMCMC with burn-in
@@ -124,7 +119,7 @@ bi <- proposal_adapted %>%
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/para5_model1131.csv")
+write.csv(bi_lst,"../data/para5_model2111.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -135,9 +130,9 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/para5_y1131.csv")
+write.csv(fitY,"../data/para5_y2111.csv")
 
-plot_df <- bi_lst$x |> mutate(value = exp(value)) %>%
+plot_df <- bi_lst$x %>% mutate(value = exp(value)) %>%
   group_by(time) %>%
   mutate(
     q025 = quantile(value, 0.025),
@@ -146,9 +141,9 @@ plot_df <- bi_lst$x |> mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/para5_beta1131.csv")
+write.csv(plot_df,"../data/para5_beta2111.csv")
 
-Mmodel <- read.csv("covidoudg2_model1.csv", header=TRUE, stringsAsFactors=FALSE)
+Mmodel <- read.csv("covidoudg2_model3.csv", header=TRUE, stringsAsFactors=FALSE)
 S<-Mmodel[-1,7]
 E<-Mmodel[-1,9]
 I<-Mmodel[-1,11]
@@ -167,7 +162,7 @@ fitS <-bi_lst$S %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(S %>% rename(S = value))
-write.csv(fitS,"../data/para5_S1131.csv")
+write.csv(fitS,"../data/para5_S2111.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -182,7 +177,7 @@ fitE <-bi_lst$E %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(E %>% rename(E = value))
-write.csv(fitE,"../data/para5_E1131.csv")
+write.csv(fitE,"../data/para5_E2111.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -197,7 +192,7 @@ fitI <-bi_lst$I %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(I %>% rename(I = value))
-write.csv(fitI,"../data/para5_I1131.csv")
+write.csv(fitI,"../data/para5_I2111.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -212,16 +207,18 @@ fitR <-bi_lst$R %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(R %>% rename(R = value))
-write.csv(fitR,"../data/para5_R1131.csv")
+write.csv(fitR,"../data/para5_R2111.csv")
 
 
-write.csv(bi_lst$k$value,"../data/para5_alpha1131.csv")
-write.csv(bi_lst$gamma$value,"../data/para5_gamma1131.csv")
-write.csv(bi_lst$sigma$value,"../data/para5_sigma1131.csv")
-write.csv(bi_lst$tau$value,"../data/para5_tau1131.csv")
-write.csv(bi_lst$theta$value,"../data/para5_theta1131.csv")
-write.csv(bi_lst$a$value,"../data/para5_a1131.csv")
-write.csv(bi_lst$b$value,"../data/para5_b1131.csv")
+write.csv(bi_lst$k$value,"../data/para5_alpha2111.csv")
+write.csv(bi_lst$gamma$value,"../data/para5_gamma2111.csv")
+write.csv(bi_lst$sigma$value,"../data/para5_sigma2111.csv")
+write.csv(bi_lst$theta$value,"../data/para5_theta2111.csv")
+write.csv(bi_lst$a$value,"../data/para5_a2111.csv")
+write.csv(bi_lst$b$value,"../data/para5_b2111.csv")
+
+
+
 
 
 
