b3a114d090b8fdb5d78f7b84f677f9bc8aea342a
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-10-27 00:14:19 +0100

    tau tunning

--- para5_11314371.R
+++ para5_11314371.R
@@ -50,7 +50,7 @@ model dureau {
     k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
     gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004)/10, 0.1, lower = 0)
-    theta ~ truncated_gaussian(0.2, 0.6, lower = 0)
+    theta ~ truncated_gaussian(0.2, 0.8, lower = 0)
     tau ~ truncated_gaussian(0.1, 0.1, lower = 0)
     a ~ truncated_gaussian(-0.02, 0.2, upper = 0)
     b ~ truncated_gaussian(-0.2, 0.3, upper = 0)
@@ -58,7 +58,7 @@ model dureau {
   
   sub proposal_parameter {
     k ~ truncated_gaussian(k, 0.01, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.05, lower = 0) 
     sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
     theta ~ truncated_gaussian(theta, 0.001, lower = 0)
     tau ~ gaussian(tau, 0.001)
