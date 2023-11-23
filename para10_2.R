218da18ed3d3e9162e4fe76a1d58a64a74419090
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-11-23 21:13:22 +0000

    real_Eng1

--- para10_2.R
+++ para10_2.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-# set.seed(0066661)
+set.seed(0066661)
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
@@ -10,6 +10,7 @@ library(rbi)
 library(rbi.helpers)
 # Load the data
 v <- read.csv("Eng_308dy.csv", header=FALSE, stringsAsFactors=FALSE) 
+
 y <- data.frame(value = v) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
   dplyr::select(time, V1)
@@ -31,7 +32,6 @@ model dureau {
   state E
   state I
   state R
-  state mu
   state x
 
   input N
@@ -41,33 +41,29 @@ model dureau {
   param gamma
   param sigma // Noise driver
   param theta
-  param a 
-  param b 
-  param tau
-
+  param a
+  param b
   
   sub parameter {
     k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
     gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
     theta ~ truncated_gaussian(0.05, 0.2, lower = 0)
-    tau ~ truncated_gaussian(0.1, 0.1, lower = 0)
     a ~ truncated_gaussian(-0.02, 0.2, upper = 0)
     b ~ truncated_gaussian(-0.2, 0.5, upper = 0)
   }
   
-  sub proposal_parameter {
+ sub proposal_parameter {
     k ~ truncated_gaussian(k, 0.01, lower = 0) 
     gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
     sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
     theta ~ truncated_gaussian(theta, 0.001, lower = 0)
-    tau ~ gaussian(tau, 0.001)
     a ~ gaussian(a, 0.001)
     b ~ gaussian(b, 0.001)
-  }
+ }
   
   sub initial {
-    x ~ gaussian(a, sigma/sqrt(2*theta) ) 
+    x ~ gaussian(a, sigma/sqrt(2*theta) )
     S <- N-1
     E <- 1
     I <- 0
@@ -77,7 +73,7 @@ model dureau {
   sub transition(delta = 1) {
     noise e
     e ~ wiener()
-    mu <- a+b*Forcing
+    inline mu = a+b*Forcing
     ode(alg = 'RK4(3)', h = 1.0, atoler = 1.0e-3, rtoler = 1.0e-8) {
       dx/dt = theta*(mu-x)+sigma*e
       dS/dt = -exp(x)*S*(0.1*I+E)/N
@@ -88,19 +84,19 @@ model dureau {
   }
 
   sub observation {
-    y ~ log_normal(log(max((E/k)/5, 0)), tau)
+    y ~ poisson((E/k)/5)
   }
 }"
-
 model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
 
 input_lst <- list(N = 56536000,Forcing=Forcing)
 end_time <- max(y$time)
 obs_lst <- list(y = y %>% dplyr::filter(time <= end_time))
+init_list <- list(k=5, gamma=9, sigma=sqrt(0.004),theta=0.05,a=-0.02,b=-0.2)
 #LibBi wrapper 
 #run launches LibBi with a particular set of command line arguments
 bi_model <- libbi(model,end_time = end_time, input = input_lst, 
-                   obs = obs_lst)
+                  init=init_list, obs = obs_lst)
 #RBi.helpers adapt_particle
 particles_adapted <- bi_model %>%
   sample(nsamples = 2000, nparticles = minParticles, 
@@ -119,7 +115,7 @@ bi <- proposal_adapted %>%
 
 bi_lst <- bi_read(bi %>% sample_obs)
 
-write.csv(bi_lst,"../data/para10_model1.csv")
+write.csv(bi_lst,"../data/para10_model2.csv")
 fitY <- bi_lst$y %>%
   group_by(time) %>%
   mutate(
@@ -130,7 +126,7 @@ fitY <- bi_lst$y %>%
     q975 = quantile(value, 0.975)
   ) %>% ungroup() %>%
   left_join(y %>% rename(Y = value))
-write.csv(fitY,"../data/para10_y1.csv")
+write.csv(fitY,"../data/para10_y2.csv")
 
 plot_df <- bi_lst$x |> mutate(value = exp(value)) %>%
   group_by(time) %>%
@@ -141,7 +137,7 @@ plot_df <- bi_lst$x |> mutate(value = exp(value)) %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(plot_df,"../data/para10_beta1.csv")
+write.csv(plot_df,"../data/para10_beta2.csv")
 
 
 S <- data.frame(value = S) %>%
@@ -156,7 +152,7 @@ fitS <-bi_lst$S %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup() 
-write.csv(fitS,"../data/para10_S1.csv")
+write.csv(fitS,"../data/para10_S2.csv")
 
 E <- data.frame(value = E) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -170,7 +166,7 @@ fitE <-bi_lst$E %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup() 
-write.csv(fitE,"../data/para10_E1.csv")
+write.csv(fitE,"../data/para10_E2.csv")
 
 I <- data.frame(value = I) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -184,7 +180,7 @@ fitI <-bi_lst$I %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup()
-write.csv(fitI,"../data/para10_I1.csv")
+write.csv(fitI,"../data/para10_I2.csv")
 
 R <- data.frame(value = R) %>%
   mutate(time = seq(1, by = 1, length.out = n())) %>%
@@ -198,16 +194,16 @@ fitR <-bi_lst$R %>%
     q75 = quantile(value, 0.75),
     q975 = quantile(value, 0.975)
   ) %>% ungroup() 
-write.csv(fitR,"../data/para10_R1.csv")
+write.csv(fitR,"../data/para10_R2.csv")
 
 
-write.csv(bi_lst$k$value,"../data/para10_alpha1.csv")
-write.csv(bi_lst$gamma$value,"../data/para10_gamma1.csv")
-write.csv(bi_lst$sigma$value,"../data/para10_sigma1.csv")
-write.csv(bi_lst$tau$value,"../data/para10_tau1.csv")
-write.csv(bi_lst$theta$value,"../data/para10_theta1.csv")
-write.csv(bi_lst$a$value,"../data/para10_a1.csv")
-write.csv(bi_lst$b$value,"../data/para10_b1.csv")
+write.csv(bi_lst$k$value,"../data/para10_alpha2.csv")
+write.csv(bi_lst$gamma$value,"../data/para10_gamma2.csv")
+write.csv(bi_lst$sigma$value,"../data/para10_sigma2.csv")
+write.csv(bi_lst$tau$value,"../data/para10_tau2.csv")
+write.csv(bi_lst$theta$value,"../data/para10_theta2.csv")
+write.csv(bi_lst$a$value,"../data/para10_a2.csv")
+write.csv(bi_lst$b$value,"../data/para10_b2.csv")
 
 
 
