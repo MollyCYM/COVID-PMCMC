8df29883e8ad93d866626dbaa9f6c95704956e76
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-11-26 15:28:33 +0000

    robtun

--- para5_113143622.R
+++ para5_113143622.R
@@ -57,13 +57,13 @@ model dureau {
   }
   
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.01, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
-    theta ~ truncated_gaussian(theta, 0.001, lower = 0)
-    tau ~ gaussian(tau, 0.001)
-    a ~ gaussian(a, 0.001)
-    b ~ gaussian(b, 0.001)
+    k ~ truncated_gaussian(k, 0.001, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.001, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.0001, lower = 0)
+    theta ~ truncated_gaussian(theta, 0.0001, lower = 0)
+    tau ~ gaussian(tau, 0.0001)
+    a ~ gaussian(a, 0.0001)
+    b ~ gaussian(b, 0.0001)
   }
   
   sub initial {
