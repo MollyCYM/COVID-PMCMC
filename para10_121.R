bb9b50d562de685e6353c02d50764fa3e678b6bb
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2023-12-11 21:14:38 +0000

    realdata4

--- para10_121.R
+++ para10_121.R
@@ -48,7 +48,7 @@ model dureau {
 
   
   sub parameter {
-    k ~ truncated_gaussian(5, 2, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
+    k ~ truncated_gaussian(5, 3, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
     gamma ~ truncated_gaussian(9, 3, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
     theta ~ truncated_gaussian(0.05, 0.2, lower = 0)
@@ -58,13 +58,13 @@ model dureau {
   }
   
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.0001, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.0001, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.00001, lower = 0)
-    theta ~ truncated_gaussian(theta, 0.00001, lower = 0)
-    tau ~ gaussian(tau, 0.00001)
-    a ~ gaussian(a, 0.00001)
-    b ~ gaussian(b, 0.00001)
+    k ~ truncated_gaussian(k, 0.00001, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.00001, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.000001, lower = 0)
+    theta ~ truncated_gaussian(theta, 0.000001, lower = 0)
+    tau ~ gaussian(tau, 0.000001)
+    a ~ gaussian(a, 0.000001)
+    b ~ gaussian(b, 0.000001)
   }
   
   sub initial {
