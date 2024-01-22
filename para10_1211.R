d23f5b117319ba7367eaede3002db63dec13104f
Molly Cui <mollycui@pc-1-83.customer.ask4.lan>
2024-01-22 13:16:40 +0000

    real3

--- para10_1211.R
+++ para10_1211.R
@@ -48,8 +48,8 @@ model dureau {
 
   
   sub parameter {
-    k ~ truncated_gaussian(5, 2, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
-    gamma ~ truncated_gaussian(9, 3, lower = 0) // gamma is the period, not the rate
+    k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
+    gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
     sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
     theta ~ truncated_gaussian(0.05, 0.2, lower = 0)
     tau ~ truncated_gaussian(0.1, 0.1, lower = 0)
