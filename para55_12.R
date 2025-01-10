bd004f868ce92eb414a20e68cd5ebc3f96af5cfd
Molly Cui <mollycui@Mollys-MacBook-Pro-5.local>
2025-01-10 14:36:24 +0000

    redo55_12

--- para55_12.R
+++ para55_12.R
@@ -36,17 +36,17 @@ model dureau {
 
   
   sub parameter {
-    k ~ truncated_gaussian(5, 1, lower = 2) // k is the period here, not the rate, i.e. 1/k is the rate
-    gamma ~ truncated_gaussian(9, 2, lower = 4) // gamma is the period, not the rate
-    sigma ~ truncated_gaussian(0.05, 0.02, lower = 0)
-    tau ~ truncated_gaussian(0.1, 0.05, lower = 0)
+    k ~ truncated_gaussian(5, 1, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
+    gamma ~ truncated_gaussian(9, 1, lower = 0) // gamma is the period, not the rate
+    sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
+    tau ~ truncated_gaussian(0.1, 0.01, lower = 0)
   }
   
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.2, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.2, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.005, lower = 0)
-    tau ~ gaussian(tau, 0.02)
+    k ~ truncated_gaussian(k, 0.01, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.0001, lower = 0)
+    tau ~ gaussian(tau, 0.001)
   }
   
   sub initial {
