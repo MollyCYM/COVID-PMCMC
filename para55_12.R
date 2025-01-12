51d94be9c2bb5f45920ba1682011ee9d1d0ccf33
Molly Cui <mollycui@Mollys-MacBook-Pro-5.local>
2025-01-12 15:36:04 +0000

    redo55_12

--- para55_12.R
+++ para55_12.R
@@ -43,10 +43,10 @@ model dureau {
   }
   
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.01, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.01, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.0001, lower = 0)
-    tau ~ gaussian(tau, 0.001)
+    k ~ truncated_gaussian(k, 0.001, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.001, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.00001, lower = 0)
+    tau ~ gaussian(tau, 0.0001)
   }
   
   sub initial {
@@ -98,8 +98,8 @@ proposal_adapted <- particles_adapted %>%
 
 #Running pMCMC with burn-in
 bi <- proposal_adapted %>%
-  sample(nsamples = 5000, thin = 1,init=init_list) %>%
-  sample(nsamples = 50000, thin = 1)
+  sample(nsamples = 1000, thin = 1,init=init_list) %>%
+  sample(nsamples = 10000, thin = 1)
 bi_lst <- bi_read(bi %>% sample_obs)
 
 fitY <- bi_lst$y %>%
