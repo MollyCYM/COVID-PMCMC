75b5d3b0bbfa4dcb5e090a4a1d32ef25380ce328
Molly Cui <mollycui@Mollys-MacBook-Pro-5.local>
2025-01-17 16:46:05 +0000

    redo55_33

--- para55_33.R
+++ para55_33.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-set.seed(0066666661)
+set.seed(0066666662)
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
@@ -36,17 +36,17 @@ model dureau {
 
   
   sub parameter {
-    k ~ truncated_gaussian(5, 2, lower = 0) // k is the period here, not the rate, i.e. 1/k is the rate
-    gamma ~ truncated_gaussian(9, 2, lower = 0) // gamma is the period, not the rate
-    sigma ~ truncated_gaussian(sqrt(0.004), 0.1, lower = 0)
-    tau ~ truncated_gaussian(0.1, 0.02, lower = 0)
+    k ~ truncated_gaussian(5, 2, lower = 1,upper=10) // k is the period here, not the rate, i.e. 1/k is the rate
+    gamma ~ truncated_gaussian(9, 2, lower = 5,upper=15) // gamma is the period, not the rate
+    sigma ~ truncated_gaussian(sqrt(0.004), 0.01, lower = 0, upper=0.1)
+    tau ~ truncated_gaussian(0.1, 0.05, lower = 0,upper=0.2)
   }
   
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.1, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.1, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
-    tau ~ gaussian(tau, 0.01)
+    k ~ truncated_gaussian(k, 0.5, lower = 1,upper=10) 
+    gamma ~ truncated_gaussian(gamma, 0.5, lower = 5,upper=15) 
+    sigma ~ truncated_gaussian(sigma, 0.01, lower = 0,upper=0.1)
+    tau ~ truncated_gaussian(tau, 0.01,lower = 0,upper=0.2)
   }
   
   sub initial {
