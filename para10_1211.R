43e77fa11718b30872d8a31520cd907dcb9a8b86
Molly Cui <mollycui@Mollys-MacBook-Pro-5.local>
2025-01-22 08:52:17 +0000

    redo10_1211

--- para10_1211.R
+++ para10_1211.R
@@ -58,13 +58,13 @@ model dureau {
   }
   
   sub proposal_parameter {
-    k ~ truncated_gaussian(k, 0.1, lower = 0) 
-    gamma ~ truncated_gaussian(gamma, 0.1, lower = 0) 
-    sigma ~ truncated_gaussian(sigma, 0.001, lower = 0)
-    theta ~ truncated_gaussian(theta, 0.001, lower = 0)
-    tau ~ gaussian(tau, 0.01)
-    a ~ gaussian(a, 0.001)
-    b ~ gaussian(b, 0.01)
+    k ~ truncated_gaussian(k, 0.05, lower = 0) 
+    gamma ~ truncated_gaussian(gamma, 0.05, lower = 0) 
+    sigma ~ truncated_gaussian(sigma, 0.0001, lower = 0)
+    theta ~ truncated_gaussian(theta, 0.0001, lower = 0)
+    tau ~ gaussian(tau, 0.001)
+    a ~ gaussian(a, 0.0001)
+    b ~ gaussian(b, 0.001)
   }
   
   sub initial {
